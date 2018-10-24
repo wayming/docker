07dbd158a1476f49a06d98872f98bdb8a6b3053a
www <wayming.z@gmail.com>
2018-10-24 22:29:17 +1000

    start container

--- rhel7/build.sh
+++ rhel7/build.sh
@@ -1,3 +1,11 @@
 docker build -t wayming/rhel7 .
 
 docker push wayming/rhel7
+
+docker ps | grep rhel7 | awk '{print $1}'  | xargs -I {} docker kill {} 
+
+docker rm rhel7
+
+docker run -t -d --name rhel7 wayming/rhel7
+
+docker ps
